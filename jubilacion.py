5ac35239eacb6795d21a2d0953081603b92467aa
JCelisR <jnnycelis@gmail.com>
2025-12-26 12:55:04 -0300

    Create jubilacion.py

---
+++ jubilacion.py
@@ -0,0 +1,14 @@
+nombre3 = "Lucía Ramírez"
+edad3 = 30
+puesto3 = "Soporte técnico"
+
+anios_restantes3 = 65 - edad3
+anio_jubilacion3 = anio_actual + anios_restantes3
+
+print("Empleado 3:")
+print("Nombre:", nombre3)
+print("Edad:", edad3)
+print("Puesto:", puesto3)
+print("Años restantes para 65:", anios_restantes3)
+print("Año estimado de jubilación:", anio_jubilacion3)
+print("-" * 40)
